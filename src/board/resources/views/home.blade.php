4bc4d26ab8d800b7c3042e921a70ca540c00a4cb
Takeyuki0617 <janjagajan0@gmail.com>
2020-10-23 02:06:56 +0900

    feat: トップページのデザイン作成

--- src/board/resources/views/home.blade.php
+++ src/board/resources/views/home.blade.php
@@ -4,14 +4,104 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('質問タイトル') }}</div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
                 <div class="card-body">
-                    <div class="card-date">2020/10/23</div>
-                    <div class="card-content">
-                        {{ __('質問内容') }}
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
                     </div>
-                    <div class="card-more-detail">詳細を見る</div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">{{ __('最近買ってよかったものを教えてください！') }}</div>
+                <div class="card-body">
+                    <div class="card-date text-right small">2020/10/23</div>
+                    <div class="card-text mb-3">
+                        {{ __('ボーナスで何か買おうと思います！ここ数か月で買ってよかったもの教えてください！') }}
+                    </div>
+                    <div class="card-more-detail text-center"><a class="btn btn-primary stretched-link" href="">この質問の回答を見る</a></div>
                 </div>
             </div>
         </div>
